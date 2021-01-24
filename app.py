c7802d457bc3babd88fdf5b8b6f744cecc498a31
JacobSeibel <seibelja@appstate.edu>
2021-01-24 18:33:14 -0500

    Whoops, re-added run.

--- app.py
+++ app.py
@@ -120,4 +120,6 @@ def getIPCounts():
 def index():
     return "<h1>Welcome to our server !!</h1>"
 
-# app.run(debug=True)
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
