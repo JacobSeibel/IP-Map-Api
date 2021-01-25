c703d969a132d3f935afd4d4fe630cc0ef165158
JacobSeibel <seibelja@appstate.edu>
2021-01-25 10:28:32 -0500

    Re-added the main logic

--- app.py
+++ app.py
@@ -125,3 +125,7 @@ def getIPCounts():
     return {"result": result}, 200
 
 readData() # Prime the cache
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
